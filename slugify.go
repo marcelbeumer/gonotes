87775a37f3ecfa4e07ddab487879476218fb9c4f
Marcel Beumer <marcel@marcelbeumer.com>
2026-01-14 00:34:15 +0100

    chore: implement frontmatter

---
+++ slugify.go
@@ -0,0 +1,19 @@
+package gonotes
+
+import (
+	"regexp"
+	"strings"
+)
+
+var (
+	reDisallowedSlugChars = regexp.MustCompile("[^a-z0-9-]")
+	reMultiDash           = regexp.MustCompile("-{2,}")
+)
+
+func Slugify(v string) string {
+	res := strings.TrimSpace(strings.ToLower(v))
+	res = reDisallowedSlugChars.ReplaceAllString(res, "-")
+	res = reMultiDash.ReplaceAllString(res, "-")
+	res = strings.Trim(res, "-")
+	return res
+}
